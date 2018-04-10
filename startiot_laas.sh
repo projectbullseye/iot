c7f58dfb53881d0dbcf048fb3e365e83a23b5995
projectbullseye <35322236+projectbullseye@users.noreply.github.com>
2018-04-10 15:30:33 -0500

    updated for interface to come up

--- startiot_laas.sh
+++ startiot_laas.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
-java -jar /home/pi/laas/iot/iotclient.jar 127.0.0.1 5019 https://www.mapquestapi.com/oauth/token RDtHEggbxGd3gWUx7o4sV1ZQxzXvMpYF k2PHauma2cx4llUr mapquest 71.115.19.60 2301 false 71.115.19.60 2400 mizaniot >> /tmp/iot.log
+STATE="error"
 
-while true
+while [ $STATE == "error" ]
 do
+    #do a ping and check that its not a default message or change to grep for something else
+    STATE=$(ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` > /dev/null && echo ok || echo error)
 
-done
+    #sleep for 2 seconds and try again
+    sleep 2
+ done
+
+java -jar /home/pi/laas/iot/iotclient.jar 127.0.0.1 5019 https://www.mapquestapi.com/oauth/token RDtHEggbxGd3gWUx7o4sV1ZQxzXvMpYF k2PHauma2cx4llUr mapquest 71.115.19.60 2301 false 71.115.19.60 2400 mizaniot >> /tmp/iot.log
