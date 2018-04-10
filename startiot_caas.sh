a1d65873a9744a1924a61ce721e86a015c0265f7
projectbullseye <35322236+projectbullseye@users.noreply.github.com>
2018-04-10 15:31:49 -0500

    updated to network to come up

--- startiot_caas.sh
+++ startiot_caas.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
+STATE="error"
+
+while [ $STATE == "error" ]
+do
+    #do a ping and check that its not a default message or change to grep for something else
+    STATE=$(ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` > /dev/null && echo ok || echo error)
+
+    #sleep for 2 seconds and try again
+    sleep 2
+ done
+
 /home/pi/rtk/RTKLIB/app/rtkrcv/gcc/rtkrcv -s -p 6060 -o /home/pi/rtk/RTKLIB/app/rtkrcv/gcc/rtkrcv.conf -t 5
