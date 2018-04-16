2ff7304d3d1b7aca5149935ad5c0db00eac30d32
projectbullseye <35322236+projectbullseye@users.noreply.github.com>
2018-04-16 13:32:50 -0500

    updated by sumanth

--- startiot_laas.sh
+++ startiot_laas.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
 #!/bin/sh
+
 STATE="error"
 
 while [ $STATE == "error" ]
@@ -10,4 +12,5 @@ do
     sleep 2
  done
 
-java -jar /home/pi/laas/iot/iotclient.jar 127.0.0.1 5019 https://www.mapquestapi.com/oauth/token RDtHEggbxGd3gWUx7o4sV1ZQxzXvMpYF k2PHauma2cx4llUr mapquest 71.115.19.60 2301 false 71.115.19.60 2400 mizaniot >> /tmp/iot.log
+java -jar /home/pi/start_at_boot/iotclient.jar 127.0.0.1 5019 https://www.mapquestapi.com/oauth/token RDtHEggbxGd3gWUx7o4sV1ZQxzXvMpYF k2PHauma2cx4llUr mapquest 71.115.19.60 2300 true 71.115.19.60 2400 Sumanthiot >>/tmp/bullseyelogs/iot.log
+
