18c28a042d9b44df12efcd69dd956d8f48391814
projectbullseye <35322236+projectbullseye@users.noreply.github.com>
2018-05-01 14:21:07 -0500

    Added files for Serial Port

---
+++ startiot_stream_serial.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+STATE="error"
+
+while [ $STATE == "error" ]
+do
+    #do a ping and check that its not a default message or change to grep for something else
+    STATE=$(ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` > /dev/null && echo ok || echo error)
+
+    #sleep for 2 seconds and try again
+    sleep 2
+ done
+
+/home/pi/start_at_boot/str2str -in serial://ttyS0:115200:N:1#ubx -out tcpsvr://127.0.0.1:5019#rtcm3 >>/tmp/bullseyelogs/stream.log
---
+++ startup_serial.sh
@@ -0,0 +1,44 @@
+
+#!/bin/bash
+set -x
+
+#Clone BUllseye Repo
+git clone https://github.com/projectbullseye/iot.git /home/pi/bullseye/
+
+#Create holding folders
+mkdir /home/pi/start_at_boot/
+mkdir /tmp/bullseyelogs/
+
+#Copy necessary files from git repo to folders
+cp /home/pi/bullseye/iotstream_serial.service /etc/systemd/system/
+cp /home/pi/bullseye/iotlaas.service /etc/systemd/system/
+cp /home/pi/bullseye/iotcaas.service /etc/systemd/system/
+cp /home/pi/bullseye/startiot_stream_serial.sh /home/pi/start_at_boot/
+cp /home/pi/bullseye/startiot_laas.sh /home/pi/start_at_boot/
+cp /home/pi/bullseye/startiot_caas.sh /home/pi/start_at_boot/
+cp /home/pi/bullseye/str2str /home/pi/start_at_boot/
+cp /home/pi/bullseye/rtkrcv /home/pi/start_at_boot/
+cp /home/pi/bullseye/iotclient.jar /home/pi/start_at_boot/
+cp /home/pi/bullseye/rtkrcv.conf /home/pi/start_at_boot/
+
+
+#Mod permissions to files for execution
+chmod 755 /home/pi/start_at_boot/str2str
+chmod 755 /home/pi/start_at_boot/rtkrcv
+chmod 755 /home/pi/start_at_boot/iotclient.jar
+chmod 755 /home/pi/start_at_boot/startiot_stream_serial.sh
+chmod 755 /home/pi/start_at_boot/startiot_laas.sh
+chmod 755 /home/pi/start_at_boot/startiot_caas.sh
+chmod 755 /home/pi/start_at_boot/rtkrcv.conf
+ 
+
+#Create services to start on boot.
+systemctl enable iotstream
+systemctl enable iotlaas
+systemctl enable iotcaas
+
+#Start services
+systemctl start iotstream
+systemctl start iotlaas
+systemctl start iotcaas
+
