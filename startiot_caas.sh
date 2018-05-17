b638b0f8a4e6df0eed1c640ff3b05dc500b2c2f8
projectbullseye <35322236+projectbullseye@users.noreply.github.com>
2018-05-17 11:36:57 -0500

    updated scripts to change ping

--- startiot_caas.sh
+++ startiot_caas.sh
@@ -4,7 +4,7 @@ STATE="error"
 while [ $STATE == "error" ]
 do
     #do a ping and check that its not a default message or change to grep for something else
-    STATE=$(ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` > /dev/null && echo ok || echo error)
+    STATE=$(ping -q -c3 google.com > /dev/null && echo ok || echo error)
 
     #sleep for 2 seconds and try again
     sleep 2
